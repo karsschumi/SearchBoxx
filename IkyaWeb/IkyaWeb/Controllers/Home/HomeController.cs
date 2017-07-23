010073f4a6b9d2bd3f0642a0a09e58b40bf2e85f
Karthik Chandrashekar <karthik.chandrashekar@harman.com>
2017-07-23 23:17:08 +0530

    added authentication 1.0

--- IkyaWeb/IkyaWeb/Controllers/Home/HomeController.cs
+++ IkyaWeb/IkyaWeb/Controllers/Home/HomeController.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using System.Web;
-using System.Web.Mvc;
 
 namespace IkyaWeb.Controllers.Home
 {
@@ -22,10 +22,57 @@ namespace IkyaWeb.Controllers.Home
         {
             return View();
         }
-        public ActionResult Login()
+        public ActionResult OpenLogin()
         {
-            return View();
+            return View("Login");
+        }
+        public void SignUp()
+        {
+            UserStore<IdentityUser> userStore = new UserStore<IdentityUser>();
+            UserManager<IdentityUser> userManager = new UserManager<IdentityUser>(userStore);
+
+            IdentityUser newUser = new IdentityUser("karthik");
+            IdentityResult userResult = userManager.Create(newUser, "karthik123");
+            if (!userResult.Succeeded)
+            {
+
+            }
+            else
+            {
+                RoleStore<IdentityRole> roleStore = new RoleStore<IdentityRole>();
+                RoleManager<IdentityRole> roleManager = new RoleManager<IdentityRole>(roleStore);
+
+                if (!roleManager.RoleExists("Administrator"))
+                {
+                    IdentityResult roleResult = roleManager.Create(new IdentityRole("Administrator"));
+                }
+               IdentityResult addRoleRsult =  userManager.AddToRole(newUser.Id, "Administrator");
+                var authManager = System.Web.HttpContext.Current.GetOwinContext().Authentication;
+                var userIdentity = userManager.CreateIdentity(newUser, DefaultAuthenticationTypes.ApplicationCookie);
+                authManager.SignIn(userIdentity);
+                Response.Redirect("~/Welcome.aspx");
+            }
+        }
+        public void Login()
+        {
+            UserManager<IdentityUser> userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>());
+            IdentityUser user = userManager.Find("karthik", "karthik123");
+            if (user == null)
+            { }
+            else
+            {
+                var authManager = System.Web.HttpContext.Current.GetOwinContext().Authentication;
+                var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                authManager.SignIn(userIdentity);
+            }
+            Response.Redirect("~/Welcome.aspx");
         }
 
+        public ActionResult LogOut()
+        {
+            var authManager = System.Web.HttpContext.Current.GetOwinContext().Authentication;
+            authManager.SignOut();
+            return View("Home");
+        }
     }
 }
---
+++ IkyaWeb/IkyaWeb/OwinStartup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity;
+
+[assembly: OwinStartup(typeof(IkyaWeb.OwinStartup))]
+
+namespace IkyaWeb
+{
+    public class OwinStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                CookieName = "SecurityCookie",
+                LoginPath = new PathString("/Login"),
+                LogoutPath = new PathString("/Login"),
+                ExpireTimeSpan = System.TimeSpan.FromMinutes(10)
+        });
+        }
+    }
+}
